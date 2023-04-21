740a1ca6fda2a47bb46c22ea700b85b9dee7f4d7
DamoFD <voshall@yahoo.com>
2023-04-20 18:01:16 -0700

    added chat rooms

--- src/components/AddFriendModal.jsx
+++ src/components/AddFriendModal.jsx
@@ -1,74 +1,131 @@
-import { Autocomplete, Dialog, DialogContent, TextField } from '@mui/material'
-import CloseIcon from "@mui/icons-material/Close"
-import React, { useCallback, useState, useEffect } from 'react'
-import GetAll from '../utils/GetAll';
+import {
+  Autocomplete,
+  Avatar,
+  Dialog,
+  DialogContent,
+  TextField,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import React, { useCallback, useState, useEffect, useContext } from "react";
+import GetAll from "../utils/GetAll";
+import { AuthContext } from "../context/AuthContext";
+import { getDoc, doc, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 function AddFriendModal({ open, handleClose }) {
-    const [error, setError] = useState(false);
-    const [username, setUsername] = useState("");
-    const [user, setUser] = useState(null);
-    const [autocompleteOptions, setAutocomopleteOptions] = useState([]);
-    const closeModal = useCallback(
-        () => {
-            setError("");
-            handleClose();
-        }, [handleClose],
-    )
+  const [error, setError] = useState(false);
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [autocompleteOptions, setAutocomopleteOptions] = useState([]);
 
-    useEffect(() => {
-        const fetchAllData = async () => {
-            const allUsers = await GetAll.fetchAllUsers();
+  const { currentUser } = useContext(AuthContext);
 
-            setAutocomopleteOptions([...allUsers]);
-        }
+  const closeModal = useCallback(() => {
+    setError("");
+    handleClose();
+  }, [handleClose]);
 
-        fetchAllData();
-    }, [])
+  useEffect(() => {
+    const fetchAllData = async () => {
+      const allUsers = await GetAll.fetchAllUsers();
 
-    const handleUsernameChange = (event, newValue) => {
-        setUsername(newValue);
+      setAutocomopleteOptions([...allUsers]);
+    };
+
+    fetchAllData();
+  }, []);
+
+  const handleUsernameChange = (event, newValue) => {
+    setUsername(newValue);
+    setUser(newValue);
+  };
+
+  const handleSubmit = async () => {
+    if (!user) return;
+
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+            [`${combinedId}.userInfo`]: {
+                uid: user.uid,
+                displayName: user.displayName,
+                photoURL: user.photoURL || '',
+            },
+            [`${combinedId}.date`]: serverTimestamp(),
+        })
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+            [`${combinedId}.userInfo`]: {
+                uid: currentUser.uid,
+                displayName: currentUser.displayName,
+                photoURL: currentUser.photoURL || '',
+            },
+            [`${combinedId}.date`]: serverTimestamp(),
+        })
+      }
+    } catch (err) {
+        console.error(err);
     }
+    closeModal();
+  };
 
   return (
     <>
-        <Dialog
-            open={open}
-            onClose={closeModal}
-            fullWidth maxWidth="sm"
+      <Dialog open={open} onClose={closeModal} fullWidth maxWidth="sm">
+        <DialogContent
+          sx={{
+            backgroundColor: "#1f2937",
+          }}
         >
-            <DialogContent
-                sx={{
-                    backgroundColor: '#1f2937',
-                }}
-            >
-                <div className='flex justify-between items-center'>
-                <div className="w-full text-center">
-                    <h1 className='text-white text-3xl'>Add a Friend</h1>
-                </div>
-                <CloseIcon onClick={ closeModal } className="cursor-pointer text-white" />
-                </div>
-                <div className="flex flex-col">
-                <Autocomplete
-                    freeSolo
-                    options={autocompleteOptions}
-                    getOptionLabel={(option) => option.displayName}
-                    onChange={handleUsernameChange}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            label='Add a Friend'
-                            variant='outlined'
-                        />
-                    )}
+          <div className="flex justify-between items-center">
+            <div className="w-full text-center">
+              <h1 className="text-white text-3xl">Add a Friend</h1>
+            </div>
+            <CloseIcon
+              onClick={closeModal}
+              className="cursor-pointer text-white"
+            />
+          </div>
+          <div className="flex flex-col">
+            <Autocomplete
+              freeSolo
+              options={autocompleteOptions}
+              getOptionLabel={(option) => option.displayName}
+              onChange={handleUsernameChange}
+              renderOption={(props, option) => (
+                <li {...props}>
+                  <Avatar
+                    src={option.photoURL}
+                    alt={option.displayName}
+                    sx={{ width: 24, height: 24, marginRight: 1 }}
+                  />
+                  {option.displayName}
+                </li>
+              )}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Add a Friend"
+                  variant="outlined"
                 />
-            <button className='text-white bg-purple-600' type="submit">
+              )}
+            />
+            <button onClick={handleSubmit} className="text-white bg-purple-600" type="submit">
               Add Friend
             </button>
-                </div>
-            </DialogContent>
-        </Dialog>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
-  )
+  );
 }
 
-export default AddFriendModal
+export default AddFriendModal;
